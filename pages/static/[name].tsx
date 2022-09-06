ccb968df64db0f4f98bbc3fab8348f6d33965201
Hyesung Park <51251258+bryce0516@users.noreply.github.com>
2022-09-06 14:57:01 +0900

    update

--- pages/static/[name].tsx
+++ pages/static/[name].tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 type Props = any;
 
-const name = ({ user, time }: any) => {
+const name = (props:any) => {
+  console.log("innner ", props)
+  const {user, time} = props
   const username = user && user.name;
   return (
     <div>
@@ -12,33 +14,54 @@ const name = ({ user, time }: any) => {
   );
 };
 
-export const getStaticProps = async ({ params }: any) => {
+name.getInitialProps = async ({query}:any) => {
+  console.log('getInitialProps',query)
   try {
-    const res = await fetch(`https://api.github.com/users/${params.name}`);
+    const res = await fetch(`https://api.github.com/users/${query.name}`);
     let result;
     if (res.status === 200) result = await res.json();
     else result = null;
+    console.log(result)
     return {
-      props: {
         user: result,
         time: new Date().toISOString(),
-      },
+
     };
   } catch (err) {
     console.warn(err);
     return {
-      props: {
         user: null,
         time: new Date().toISOString(),
-      },
     };
   }
-};
+}
+// export const getStaticProps = async ({ params }: any) => {
+//   try {
+//     const res = await fetch(`https://api.github.com/users/${params.name}`);
+//     let result;
+//     if (res.status === 200) result = await res.json();
+//     else result = null;
+//     return {
+//       props: {
+//         user: result,
+//         time: new Date().toISOString(),
+//       },
+//     };
+//   } catch (err) {
+//     console.warn(err);
+//     return {
+//       props: {
+//         user: null,
+//         time: new Date().toISOString(),
+//       },
+//     };
+//   }
+// };
 
-export const getStaticPaths = async () => {
-  return {
-    paths: [{ params: { name: 'bryce0516' } }],
-    fallback: true,
-  };
-};
+// export const getStaticPaths = async () => {
+//   return {
+//     paths: [{ params: { name: 'bryce0516' } }],
+//     fallback: true,
+//   };
+// };
 export default name;
--- pages/user/[name].tsx
+++ pages/user/[name].tsx
@@ -10,13 +10,13 @@ export const getServerSideProps: GetServerSideProps<
   any,
   ParsedUrlQuery,
   PreviewData
-> = async ({query}) => {
+> = async ({ query }) => {
   try {
-    console.log("this is query", query.name)
+    console.log('this is query', query.name);
     const res = await fetch(`https://api.github.com/users/${query.name}`);
     let result;
     if (res.status === 200) result = await res.json();
-    else result = null
+    else result = null;
     return {
       props: {
         user: result,
@@ -37,7 +37,48 @@ const name = (props: Props) => {
   const router = useRouter();
 
   console.log(props);
-  return <div>name</div>;
+  return (
+    <>
+      <div style={styles.profileBox}>
+        <div style={styles.profileImageWrapper}>
+          <img style={styles.profileImage} src={user.avatar_url}/> 
+        </div>
+        <h2 style={styles.profileUserName}>{user.name}</h2>
+        <h2 style={styles.profileUserLogin}>{user.login}</h2>
+        <h2 style={styles.profileUserBio}>{user.bio}</h2>
+      </div>
+    </>
+  );
 };
 
+const styles = {
+  profileBox: {
+    width: "25%",
+    maxWith: 272,
+    marginRight: 26
+  },
+  profileImageWrapper: {
+    width:"100%",
+    border: "1px solid #ele4e8"
+  },
+  profileImage: {
+    display:"block",
+    width:"100%"
+  },
+  profileUserName: {
+    margin:0,
+    paddingTop: 16,
+    fontSize: 26
+  },
+  profileUserLogin: {
+    margin: 0,
+    fontSize: 20
+  },
+  profileUserBio : {
+    margin: 0,
+    paddingTop: 16,
+    fontSize: 14
+  }
+
+}
 export default name;
